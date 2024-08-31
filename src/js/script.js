8634980e21e63e72b057fc529f243c7b0d1b4b19
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-08-31 19:56:03 +0330

    worked

--- src/js/script.js
+++ src/js/script.js
@@ -3,54 +3,57 @@ const header = document.querySelector(".header");
 const newHeight = Math.abs(header.clientHeight - window.innerHeight);
 hero.style.height = `${newHeight}px`;
 
-
-
 const API = "https://valorant-api.com/v1/agents";
 
-getAgents()
+getAgents();
 async function getAgents() {
   const res = await fetch(API);
   const json = await res.json();
-  
+
   return json;
-};
+}
 
 async function setData() {
-    const json = await getAgents();
-    console.log(json.data)
- 
-    json.data.map(item => {
-        const root = document.querySelector('.slider-wrapper');
-        const slide = document.createElement('div');
-        slide.className = 'slide';
-        const slideContainer = document.createElement('div');
-        slideContainer.className = 'slide-container';
-        const agentsName = document.createElement('h3');
-        agentsName.className = 'agent-name';
-        agentsName.innerHTML = item.displayName;
-        const agentImage = document.createElement('img');
-        agentImage.className = 'agent-img';
-        agentImage.src = item.fullPortraitV2;
-        const agentPowerContainer = document.createElement('div');
-        agentPowerContainer.className = 'powers-logo';
-        const agentPowerImg = document.createElement('img');
-        agentPowerImg.className = 'powers-logo';
+  const json = await getAgents();
+  console.log(json.data);
+
+  json.data.map((item) => {
+    if(item.fullPortraitV2 === null) return false;
     
-        slide.appendChild(slideContainer);
-        slideContainer.appendChild(agentsName);
-        slideContainer.appendChild(agentImage);
-        slideContainer.appendChild(agentPowerContainer);
+    const root = document.querySelector(".slider-wrapper");
+    const slide = document.createElement("div");
+    slide.className = "slide";
+    const slideContainer = document.createElement("div");
+    slideContainer.className = "slide-container";
+    const agentsName = document.createElement("h3");
+    agentsName.className = "agent-name";
+    agentsName.innerHTML = item.displayName;
+    const agentImageWrapper = document.createElement("div");
+    agentImageWrapper.className = "agent-image-wrapper";
+    const agentImage = document.createElement("img");
+    agentImage.className = "agent-img";
+    agentImage.src = item.fullPortraitV2;
+    const agentPowerContainer = document.createElement("div");
+    agentPowerContainer.className = "powers-logo";
+
+
+    item.abilities.map((abi, index) => {
+      if (index < 4) {
+        const agentPowerImg = document.createElement("img");
+        agentPowerImg.style.width = "30px";
+        agentPowerImg.src = abi.displayIcon;
+
         agentPowerContainer.appendChild(agentPowerImg);
-        root.appendChild(slide)
+      }
+    });
 
-    })
 
+    slide.appendChild(slideContainer);
+    slideContainer.appendChild(agentsName);
+    agentImageWrapper.appendChild(agentImage);
+    slideContainer.appendChild(agentImageWrapper);
+    slideContainer.appendChild(agentPowerContainer);
+    root.appendChild(slide);
+  });
 }
-setData()
-
-
-
-
-
-
-
+setData();
